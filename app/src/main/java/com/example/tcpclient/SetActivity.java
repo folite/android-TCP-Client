9fccbaa45ec7345f7e07ea6a46c563610287623c
folite <zp159123@gmail.com>
2017-07-20 17:25:35 +0800

    add layout

--- app/src/main/java/com/example/tcpclient/SetActivity.java
+++ app/src/main/java/com/example/tcpclient/SetActivity.java
@@ -2,13 +2,24 @@
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class SetActivity extends AppCompatActivity {
+ public class SetActivity extends AppCompatActivity {
+    EditText editTextIPPort;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set);
+
+        editTextIPPort = (EditText) findViewById(R.id.editTextIPPort);
+    }
+
+    public void connection(View v){
+        Toast.makeText(getApplicationContext(),editTextIPPort.getText(),Toast.LENGTH_SHORT).show();
     }
 
 }
