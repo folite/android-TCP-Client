5be810216afbb52dd54b08608489299121f0f7ff
folite <zp159123@gmail.com>
2017-07-20 21:29:12 +0800

    upback

--- app/src/main/java/com/example/tcpclient/SetActivity.java
+++ app/src/main/java/com/example/tcpclient/SetActivity.java
@@ -1,5 +1,6 @@
  package com.example.tcpclient;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,17 +10,34 @@ import android.widget.Toast;
 
  public class SetActivity extends AppCompatActivity {
     EditText editTextIPPort;
-
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set);
-
+        context = this;
         editTextIPPort = (EditText) findViewById(R.id.editTextIPPort);
+
+
     }
 
     public void connection(View v){
-        Toast.makeText(getApplicationContext(),editTextIPPort.getText(),Toast.LENGTH_SHORT).show();
+        String data[] = editTextIPPort.getText().toString().split(":");
+        Thread TTCPClient = new Thread(new ThreadTCPClient(data[0], data[1]));
+        runOnUiThread(TTCPClient);
     }
+    class ThreadTCPClient implements Runnable{
+        private String IP,Port;
 
+        public ThreadTCPClient(String IP, String Port){
+            this.IP = IP;
+            this.Port =  Port;
+        }
+
+        @Override
+        public void run() {
+
+            Toast.makeText(getApplicationContext(), IP + ":" + Port, Toast.LENGTH_SHORT).show();
+        }
+     }
 }
